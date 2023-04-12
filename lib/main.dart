96cf4448849612df9a87f0d14290f7d4062ab1ac
Bruno Silveira <silverdev42@gmail.com>
2023-04-12 12:34:39 -0300

    Added time options and better buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,135 +1,144 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 final player = AudioPlayer();
-final fullDuration = Stopwatch();
-final everyTempo = Stopwatch();
+final durationcounter = Stopwatch();
+final beatcounter = Stopwatch();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+          title: 'Flutter Demo Home Page', bpm: '60', duration: '10'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage(
+      {super.key,
+      required this.title,
+      required this.bpm,
+      required this.duration});
 
   final String title;
+  final String bpm;
+  final String duration;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void beatcontrol(String action, String pduration, String pbpm) async {
+    var bpm = int.parse(pbpm);
+    var duration = int.parse(pduration);
 
-  Future play() async {
-    fullDuration.start();
-    everyTempo.start();
-    while (fullDuration.elapsedMilliseconds < 10000) {
-      while (everyTempo.elapsedMilliseconds < 1000) {
+    durationcounter.reset();
+    beatcounter.reset();
+    durationcounter.start();
+    beatcounter.start();
+
+    //TODO: find way to create beep instead of play mp3 due to delay.
+
+    await player.setSource(AssetSource('metronome_basic_sound.mp3'));
+    await player.resume();
+    while (durationcounter.elapsedMilliseconds <= 1000 * duration) {
+      if (beatcounter.elapsedMilliseconds == (60 / bpm) * 1000) {
         await player.setSource(AssetSource('metronome_basic_sound.mp3'));
         await player.resume();
+        beatcounter.reset();
       }
-      everyTempo.reset();
     }
-    fullDuration.reset();
-    fullDuration.stop();
-    everyTempo.stop();
+
+    durationcounter.reset();
+    durationcounter.stop();
+    beatcounter.stop();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  TextEditingController contbpm = TextEditingController();
+  TextEditingController contduration = TextEditingController();
+
+  @override
+  void initState() {
+    contbpm.text = widget.bpm;
+    contduration.text = widget.duration;
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: play,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Press this button to start the metronome:',
+              ),
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'Duração do metrônomo',
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: contduration,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                  ],
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'BPM',
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: contbpm,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                  ],
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () =>
+                        beatcontrol("start", contduration.text, contbpm.text),
+                    tooltip: 'Play',
+                    child: const Icon(Icons.play_arrow),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () => beatcontrol("stop", "0", "0"),
+                    tooltip: 'Stop',
+                    child: const Icon(Icons.stop),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
